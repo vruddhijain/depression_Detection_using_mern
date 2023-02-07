5f232a10003761d9af5955ff8caaec10808af419
vruddhijain <2020an0120823@gmail.com>
2023-02-07 09:14:46 +0530

    depro

---
+++ quiz.js
@@ -0,0 +1,297 @@
+//Question bank
+var questionBank= [
+    {
+        question : 'Do you express your feeling to someone or something u love?',
+        option : ['family & friends','I prefer pets', 'non -living things', 'I keep things to myself'],
+        answer1 : 'I keep things to myself',  //3
+        answer2 : 'non -living things',  //2
+        answer3 : 'I prefer pets', //1
+        answer4 : 'family & friends' //0
+    },
+    {
+        question : 'How do you spend most of your time?',
+        option : ['sleeping', 'social media addict', 'working', 'hobbies'],
+        answer1 : 'sleeping',  //3
+        answer2 : 'social media addict',  //2
+        answer3 : 'working', //1
+        answer4 : 'hobbies' //0
+    },
+    // {
+    //     question : 'Are you addicted to drugs?(alcohol etc)',
+    //     option : ['I consume on regular basis', 'not addicted' , 'weakly addicted', 'occasional consumer'],
+    //     answer1 : 'I consume on regular basis',  //3
+    //     answer2 : 'weakly addicted',  //2
+    //     answer3 : 'occasional consumer', //1
+    //     answer4 : 'not addicted' //0
+    // },
+    // {
+    //     question : 'You feel lonely even if you are surrounded by lots of people?',
+    //     option : ['I am comfortable only with people i know','I like interacting with people', "feel like leaving the place", "introvert by choice"],
+    //     answer1 : "feel like leaving the place",  //3
+    //     answer2 : "introvert by choice",  //2
+    //     answer3 : 'I am comfortable only with people i know', //1
+    //     answer4 : 'I like interacting with people' //0
+    // },
+    {
+        question : 'Do you fall into other thoughts when you are amidst something?',
+        option : ['Usually if the conversation is boring','I have low attention', 'I cannot focus on things when my mind is unstable', 'I try to get out of that world though i am lost'],
+        answer1 : 'I cannot focus on things when my mind is unstable',  //3
+        answer2 : 'I have low attention',  //2
+        answer3 : 'I try to get out of that world though i am lost', //1
+        answer4 : 'Usually if the conversation is boring' //0
+    },
+    // {
+    //     question : 'Are you financialy stable? ',
+    //     option : ['Yes','No, hardly trying to cope up','i am dependent', "that was never the issue"],
+    //     answer1 : 'No, hardly trying to cope up',  //3
+    //     answer2 : 'i am dependent',  //2
+    //     answer3 : 'Yes', //1
+    //     answer4 : "that was never the issue" //0
+    // },
+    // {
+    //     question : 'How much do you value promises?',
+    //     option : ['Depends on the person whom the promises are made to', 'I don’t believe in promises', 'Used to do it until I got betrayed', 'I always keep my promises up'],
+    //     answer1 : 'Used to do it until I got betrayed',  //3
+    //     answer2 : 'Depends on the person whom the promises are made to',  //2
+    //     answer3 : 'I always keep my promises up', //1
+    //     answer4 : "I don’t believe in promises" //0
+    // },
+    // {
+    //     question : 'How do you express your feelings?',
+    //     option : ['by crying','by writing a diary','i prefer keeping feelings to myself','sharing it with my people'
+    //         ],
+    //     answer1 : 'i prefer keeping feelings to myself',  //3
+    //     answer2 : 'by crying',  //2
+    //     answer3 : 'by writing a diary', //1
+    //     answer4 : 'sharing it with my people' //0
+    // },
+    {
+        question : 'Are u suffering from any kind of diseases?',
+        option : ['yes, its curable','yes, its non curable','no, I am healthy','i am unhealthy because of my age'
+            ],
+        answer1 : 'yes, its non curable',  //3
+        answer2 : 'yes, its curable',  //2
+        answer3 : 'i am unhealthy because of my age', //1
+        answer4 : 'no, I am healthy' //0
+
+    },
+    // {
+    //     question : 'How is your relationship status?',
+    //     option : ["single",
+    //         "happily commited",
+    //         "committed but have issues",
+    //         "can't disclose"
+    //         ],
+    //     answer1 : "committed but have issues",  //3
+    //     answer2 : "can't disclose",  //2
+    //     answer3 : "single", //1
+    //     answer4 : "happily commited" //0
+
+    // },
+    {
+        question : "Are you trying to manifest everything you fear of?"
+        ,
+        option : ['yes','no','depends on how I tackle',"not all the time"
+            ],
+        answer1 : 'no',  //3
+        answer2 : "not all the time",  //2
+        answer3 : 'depends on how I tackle', //1
+        answer4 : 'yes' //0
+
+    },
+
+    // {
+    //     question : 'How many hours of nap do you take everyday?',
+    //     option : ['2-4','4-6','6-8','8-10'],
+    //     answer1 : '2-4',  //3
+    //     answer2 : '4-6',  //2
+    //     answer3 : '6-8', //1
+    //     answer4 : '8-10' //0
+    // },
+{
+        question : 'Do you feel guilty even if you are not wrong?',
+        option : ['yes','strongly yes','cannot get over it','no'],
+        answer1 : 'cannot get over it ',  //3
+        answer2 : 'strongly yes',  //2
+        answer3 : 'yes', //1
+        answer4 : 'no' //0
+    },
+{
+        question : 'How do you feel when someone makes efforts to make happy?',
+        option : ['pleased',"i don't like relying on others",'i look forward to please them too','pretend to be pleased'],
+        answer1 : "i don't like relying on others ",  //3
+        answer2 : 'pretend to be pleased',  //2
+        answer3 : 'pleased', //1
+        answer4 : 'i look forward to please them too' //0
+    },
+{
+        question : 'Are you feeling being left out by the are people who are very close to you?',
+        option : ['yes','no','sometimes','depends on environment'],
+        answer1 : 'yes ',  //3
+        answer2 : 'somtimes',  //2
+        answer3 : 'depends on environment', //1
+        answer4 : 'no' //0
+    },
+{
+        question : 'Have you ever been betrayed by the person whom you thought they would never break you?',
+        option : ["yes i could'nt get over it",'never','i am going through it','i got over it'],
+        answer1 : 'i am going through it',  //3
+        answer2 : "yes i could'nt get over it",  //2
+        answer3 : 'i got over it', //1
+        answer4 : 'never' //0
+    },
+{
+        question : 'Recurring thoughts of deaths or suicide thoughts haunt you everytime?',
+        option : ['yes','i have never got that thought','sensed in past','currently having'],
+        answer1 : 'yes',  //3
+        answer2 : 'currently having',  //2
+        answer3 : 'sensed in past', //1
+        answer4 : 'i have never got that thought' //0
+    },
+{
+        question : 'you always fake your smile so that your grief should not get noticed?',
+        option : ['i always do','sometimes','i prefer showing my true emotions','i am used to,it'],
+        answer1 : 'i am used to it',  //3
+        answer2 : 'i prefer showing my true emotions',  //2
+        answer3 : 'i always do', //1
+        answer4 : 'sometimes' //0
+    },
+{
+        question : 'you find yourself  not interested in any of the things you were interested in before? ',
+        option : ['yes i have given upon my hobbies','yes','may be','no,but still i continue'],
+        answer1 : 'yes i have given upon my hobbies',  //3
+        answer2 : 'yes',  //2
+        answer3 : 'may be', //1
+        answer4 : 'no,but still i continue' //0
+    },
+{
+        question : 'do you get distracted easily?',
+        option : ['very often','no','depends on what i am going through','sometimes'],
+        answer1 : 'very often',  //3
+        answer2 : 'depends on what i am going through',  //2
+        answer3 : 'sometimes', //1
+        answer4 : 'no' //0
+    },
+{
+        question : 'Do you get anxious or irritated by little things? ',
+        option : ['very often','no','depends on what i am going through','sometimes'],
+        answer1 : 'very often',  //3
+        answer2 : 'depends on what i am going through',  //2
+        answer3 : 'sometimes', //1
+        answer4 : 'no' //0
+    }
+
+   
+]
+
+var question= document.getElementById('question');
+var quizContainer= document.getElementById('quiz-container');
+var scorecard= document.getElementById('scorecard');
+var option0= document.getElementById('option0');
+var option1= document.getElementById('option1');
+var option2= document.getElementById('option2');
+var option3= document.getElementById('option3');
+var next= document.querySelector('.next');
+var points= document.getElementById('score');
+var span= document.querySelectorAll('span');
+var i=0;
+var score= 0;
+var percent;
+var percent1;
+
+//function to display questions
+function displayQuestion(){
+    for(var a=0;a<span.length;a++){
+        span[a].style.background='none';
+    }
+    question.innerHTML= 'Q.'+(i+1)+' '+questionBank[i].question;
+    option0.innerHTML= questionBank[i].option[0];
+    option1.innerHTML= questionBank[i].option[1];
+    option2.innerHTML= questionBank[i].option[2];
+    option3.innerHTML= questionBank[i].option[3];
+    stat.innerHTML= "Question"+' '+(i+1)+' '+'of'+' '+questionBank.length;
+}
+
+//function to calculate scores
+function calcScore(e){
+    if(e.innerHTML===questionBank[i].answer1)
+    {
+        score= score+3;
+        document.getElementById(e.id).style.background= 'limegreen';
+    }
+    else if(e.innerHTML===questionBank[i].answer2)
+    {
+        score= score+2;
+        document.getElementById(e.id).style.background= 'limegreen';
+    }
+    else if(e.innerHTML===questionBank[i].answer3)
+    {
+        score= score+1;
+        document.getElementById(e.id).style.background= 'limegreen';
+    }
+    
+    else
+    {
+        score= score+0;
+        document.getElementById(e.id).style.background= 'limegreen';
+    }
+
+    percent = (score/40)*100;
+    percent1 = percent.toFixed();
+
+    // else{
+    //     document.getElementById(e.id).style.background= 'tomato';
+    // }
+
+
+    setTimeout(nextQuestion,300);
+}
+
+function levelD(percent1){
+    if(percent1>=45){
+        windows.alert("You are in depression,Please do visit level 2 page of our website");
+    
+
+    }
+    else{
+        windows.alert("You do have mild symptoms of depression,Please do visit level 1 page of our website");
+    }
+}
+//function to display next question
+function nextQuestion(){
+    if(i<questionBank.length-1)
+    {
+        i=i+1;
+        displayQuestion();
+    }
+    else{
+        points.innerHTML= percent1 + "%";
+        quizContainer.style.display= 'none';
+        scoreboard.style.display= 'block'
+    }
+}
+
+//click events to next button
+next.addEventListener('click',nextQuestion);
+
+//Back to Quiz button event
+function backToQuiz(){
+    location.reload();
+}
+
+// //function to check Answers
+function checkAnswer(){
+    //var answerBank= document.getElementById('answerBank');
+    var answers= document.getElementById('answers');
+   // answerBank.style.display= 'block';
+    scoreboard.style.display= 'none';
+    for(var a=0;a<questionBank.length;a++)
+    {
+        var list= document.createElement('li');
+        list.innerHTML= questionBank[a].answer;
+        answers.appendChild(list);
+    }
+}
+
+
+displayQuestion();
